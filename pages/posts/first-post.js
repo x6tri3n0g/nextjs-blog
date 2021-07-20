9730fedd85a9b18f104a9d70545aed58d96f9a38
xtring <hh940630@gmail.com>
2021-07-20 19:51:29 +0900

    add: Head Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,25 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+	return (
+		<>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<h1>First Post</h1>
+			<Image
+				src='/images/profile.jpg'
+				width={144}
+				height={144}
+				alt='Your Name'
+			/>
+			<h2>
+				<Link href='/'>
+					<a>Back to home</a>
+				</Link>
+			</h2>
+		</>
+	);
 }
