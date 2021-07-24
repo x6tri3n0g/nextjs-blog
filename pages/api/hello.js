2e1d1386df6ec67eb89f9a8d8fe743deb4706589
xtring <hh940630@gmail.com>
2021-07-24 23:41:23 +0900

    Add: api des

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,3 +4,6 @@ export default function handler(req, res) {
 }
 
 // Try accssing it at http://localhost:3000/api/hello.
+
+// 이 방식을 통해 getStaticProps나 getStaticPaths를 사용하지 않는다.
+// 이 두 방식은 server-side에서만 동작한다.
