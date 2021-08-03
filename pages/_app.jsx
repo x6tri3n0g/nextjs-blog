5570be4156abd8cc9aa672d8f36ea18b4098d67a
xtring <hh940630@gmail.com>
2021-08-03 18:26:07 +0900

    feat/build: 빌드 환경 세팅 -빌드환경 별 API Domain 변경할 수 있도록 구성함

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,5 +3,6 @@ import React from 'react';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
+	console.log(process.env.NEXT_PUBLIC_TEST);
 	return <Component {...pageProps} />;
 }
